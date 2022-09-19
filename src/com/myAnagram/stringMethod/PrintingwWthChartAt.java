0f22aa01a07c45137128cdfa90b6852da1fd23f7
German <barreragerman27@gmail.com>
2022-09-19 13:39:49 -0300

    printing the number of characters using chartAtArray().

--- src/com/myAnagram/stringMethod/PrintingwWthChartAt.java
+++ src/com/myAnagram/stringMethod/PrintingwWthChartAt.java
@@ -3,6 +3,14 @@ package com.myAnagram.stringMethod;
 public class PrintingwWthChartAt {
     public static void main(String[] args) {
         String x = "Luis German Barrera";
-        System.out.println(x.length());
+       // System.out.println(x.length());
+
+        char[] d = x.toCharArray();
+        int count = 0;
+
+        for (int i = 0; i < x.length(); i++) {
+            count++;
+        }
+        System.out.println(count);
     }
 }
